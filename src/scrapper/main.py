803f82beecc7ea7705cbebaf491eb84938aa3742
Yan Carvalho <32984965+yancborges@users.noreply.github.com>
2021-08-28 19:32:39 -0300

    Finishing basic scrapping
    Finished the main data scrap system; Added output file

--- src/scrapper/main.py
+++ src/scrapper/main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from operation import Operation
+import pandas as pd
 
 def scrap_page():
     """
@@ -12,10 +13,36 @@ def scrap_page():
     items = raw_content.find_all('li')
 
     # Ignoring elements with any HTML attribute set
-    items = [i for i in items if not i.attrs]
+    items = [
+        i for i in items 
+            if not i.attrs 
+            and (
+                (
+                    'style' in i.parent.parent.attrs 
+                    and i.parent.parent.attrs['style'].startswith('-moz-column-count')
+                )
+                or
+                (
+                    'class' in i.parent.parent.attrs
+                    and i.parent.parent.attrs['class'] == ['mw-parser-output']
+                )
+                or
+                (
+                    i.parent.name == 'ul'
+                    and i.parent.parent.name == 'li'
+                )
+            )
+            and not (
+                'class' in i.parent.attrs
+                and i.parent.attrs['class'] == 'references'
+            )
+    ]
     for i in items:
         operations.append(
             Operation().mount_from_bs4(i)
         )
 
-scrap_page()
+    return operations
+
+operations = [i.to_json() for i in scrap_page()]
+pd.DataFrame(operations).to_csv('pf_operations.csv', sep=',')
--- src/scrapper/operation.py
+++ src/scrapper/operation.py
@@ -1,6 +1,7 @@
 from os import name
 from bs4.element import Tag
 import re
+import inspect
 
 class Operation:
     """
@@ -15,27 +16,46 @@ class Operation:
         self.tags = []
 
 
+    def to_json(self):
+        raw_attributes = inspect.getmembers(self, lambda a:not(inspect.isroutine(a)))
+        clean_attributes = [
+            a for a in raw_attributes if not(a[0].startswith('_') or a[0].endswith('__'))
+        ]
+
+        return {attr[0]: attr[1] for attr in clean_attributes}
+
+
     def __normalize(self):
-        name_rgx = '\([\w\s\d]+\)'
+        name_rgx = '\(.+\)'
         roman_rgx = '[IVXLCDM]+$'
-        self.name = re.sub(re.compile(name_rgx), self.name, '')
-        self.name = re.sub(re.compile(roman_rgx), self.name, '')
+        self.name = re.sub(re.compile(name_rgx), '', self.name)
+        #self.name = re.sub(re.compile(roman_rgx), '', self.name)
+        self.name = self.name.strip()
+
+        self.aliases = [
+            re.sub(r'^\s?ou\s', '', a).split('(')[0].replace(',', '').strip() for a in self.aliases
+        ]
+        self.aliases = list(set([a for a in self.aliases if a]))
 
 
     def mount_from_bs4(self, bs4_tag):
         for c in bs4_tag.contents:
-            if isinstance(c, str):
+            if isinstance(c, str) and not self.name:
                 self.name = c
+            elif isinstance(c, str) and self.name:
+                self.aliases.append(c)
             else:
                 if 'href' in c.attrs:
-                    self.name = c.next
                     self.direct_url = c.attrs['href']
-                else:
-                    self.name = c.parent.contents[0].next
-                    if c.name == 'sup':
-                        self.related_url = c.next['href']
+                    self.name = c.attrs['title']
+                elif c.name == 'sup':
+                    self.related_url = c.next['href']
+            
+            if c.parent.parent.parent.name == 'li':
+                self.aliases.append(c.parent.parent.parent.contents[0].next)          
 
         self.__normalize()
+        return self
         
 
 
